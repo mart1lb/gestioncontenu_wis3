f7afc093230a7f7ec316e36733f19e6c3ecf8bdf
Mart1 <martinlb28@gmail.com>
2022-03-10 11:49:34 +0100

    création cpt recrutement

--- inc/theme-custom-post-types.php
+++ inc/theme-custom-post-types.php
@@ -2,7 +2,7 @@
 
 
 function lazuli_custom_post_types() {
-	$cpt = array(
+	$cpt1 = array(
 		'labels'              => array(
 			'name'          => __( 'Etablissement', 'lazuli' ),
 			'singular_name' => __( 'Etablissements', 'lazuli' ),
@@ -19,7 +19,26 @@ function lazuli_custom_post_types() {
 		'menu_icon'           => 'dashicons-welcome-learn-more',
 	);
 
-	register_post_type( 'etablissement_cpt', $cpt );
+    $cpt2 = array(
+        'labels'              => array(
+            'name'          => __( 'recrutement', 'lazuli' ),
+            'singular_name' => __( 'recrutement', 'lazuli' ),
+        ),
+        'show_ui'             => true,
+        'show_admin_column'   => true,
+        'show_in_rest'        => true,
+        'hierarchical'        => true,
+        'public'              => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'rewrite'             => array( 'slug' => 'recruit' ),
+        'supports'            => array( 'title', 'editor', 'thumbnail', 'page-attributes', 'excerpt' ),
+        'menu_icon'           => 'dashicons-welcome-learn-more',
+    );
+
+	register_post_type( 'etablissement', $cpt1 );
+
+    register_post_type( 'recrutement', $cpt2 );
 }
 
 add_action( 'init', 'lazuli_custom_post_types' );
